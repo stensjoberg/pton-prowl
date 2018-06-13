edf921adbc1825fd70134becb8351163a353d623
Sten Sjöberg <stenogsjoberg@gmail.com>
2018-06-13 12:32:09 -0400

    extended polls' model, enabled admin access

--- web/ptonprowler/polls/admin.py
+++ web/ptonprowler/polls/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-
+from .models import Question
 # Register your models here.
 
+<<<<<<< Updated upstream
 from .models import Question
 
+=======
+>>>>>>> Stashed changes
 admin.site.register(Question)
--- web/ptonprowler/polls/models.py
+++ web/ptonprowler/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+<<<<<<< Updated upstream
 from django.db import models
 from django.utils import timezone
 import datetime
@@ -16,10 +19,35 @@ class Question(models.Model):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
+=======
+from django.utils import timezone
+
+# Create your models here.
+
+# questions class
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+    def was_published_recently(self):
+            return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.question_text
+
+# choices class
+>>>>>>> Stashed changes
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+<<<<<<< Updated upstream
     # to string method
     def __str__(self):
         return self.choice_text
+=======
+
+    def __str__(self):
+            return self.choice_text
+>>>>>>> Stashed changes
