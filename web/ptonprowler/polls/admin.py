33620d1bc8c210867e771f77bd0579cd6751a670
Sten Sjöberg <stenogsjoberg@gmail.com>
2018-06-13 13:38:58 -0400

    rectified merge issues

--- web/ptonprowler/polls/admin.py
+++ web/ptonprowler/polls/admin.py
@@ -2,9 +2,4 @@ from django.contrib import admin
 from .models import Question
 # Register your models here.
 
-<<<<<<< Updated upstream
-from .models import Question
-
-=======
->>>>>>> Stashed changes
 admin.site.register(Question)
--- web/ptonprowler/polls/models.py
+++ web/ptonprowler/polls/models.py
@@ -1,34 +1,12 @@
 import datetime
 
 from django.db import models
-<<<<<<< Updated upstream
-from django.db import models
 from django.utils import timezone
-import datetime
-
-# Create your models here.
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    # to string method
-    def __str__(self):
-        return self.question_text
-    # custom method
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
-=======
-from django.utils import timezone
-
-# Create your models here.
 
 # questions class
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-
 
     def was_published_recently(self):
             return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
@@ -37,17 +15,10 @@ class Question(models.Model):
         return self.question_text
 
 # choices class
->>>>>>> Stashed changes
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-<<<<<<< Updated upstream
     # to string method
     def __str__(self):
         return self.choice_text
-=======
-
-    def __str__(self):
-            return self.choice_text
->>>>>>> Stashed changes
