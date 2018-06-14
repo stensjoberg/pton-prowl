ed0321760aaadef03e8865efe76b25553f9d4202
Sten Sjöberg <stenogsjoberg@gmail.com>
2018-06-14 11:13:58 -0400

    fixed keys import bug

--- web/ptonprowl/ptonprowl/settings/base.py
+++ web/ptonprowl/ptonprowl/settings/base.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Imports all keys from hidden keys file
-from .keys import *
-
+from . import keys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
