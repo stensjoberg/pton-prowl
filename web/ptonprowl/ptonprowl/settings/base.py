1d1fbcf95d038d727fe91362fd5c270b5a5cb309
Sten Sjöberg <stenogsjoberg@gmail.com>
2018-06-14 12:21:21 -0400

    made prowl app

--- web/ptonprowl/ptonprowl/settings/base.py
+++ web/ptonprowl/ptonprowl/settings/base.py
@@ -70,10 +70,15 @@ WSGI_APPLICATION = 'ptonprowl.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# TODO: Change to mySQL database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'prowl_db',
+        'USER': 'root',
+        'PASSWORD': keys.ROOT_PW,
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
