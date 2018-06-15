cd65f7c928d054505dc3e84b0e4cdadab3d4739d
Sten Sjöberg <stenogsjoberg@gmail.com>
2018-06-15 16:08:54 -0400

    incomplete: adding profile view

--- web/ptonprowl/students/views.py
+++ web/ptonprowl/students/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+from .models import Student
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+class AccountDetails(generic.DetailView):
+    model = Student
+    template_name = 'students/account.html'
+
+    def get_object(self):
+            return get_object_or_404(Student, netid=self.kwargs.get('netid'))
