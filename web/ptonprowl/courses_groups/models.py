8db00b378b94da4335028ebfe1d607cda480ea66
Van Brantley <van.brantley@gmail.com>
2018-06-19 13:58:15 -0400

    updated courses_groups model

--- web/ptonprowl/courses_groups/models.py
+++ web/ptonprowl/courses_groups/models.py
@@ -3,15 +3,13 @@ from django.db import models
 # course model
 
 class Course(models.Model):
-    number = models.CharField(max_length = 6, unique = True)
-    # courseid = models.CharField(max_length=6, unique=True)
-    name = models.CharField(max_length=200, unique=True)
+    number = models.IntegerField(default=0, primary_key=True)
+    title = models.CharField(max_length=200, unique=False)
     groups = models.TextField()
 
 class CourseID(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
 class Group(models.Model):
-    groupid =  models.CharField(max_length=3, unique=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     members = models.TextField()
