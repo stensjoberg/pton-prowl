e65142f94c42f4a8adb479e39f7448b70075d226
Van Brantley <van.brantley@gmail.com>
2018-06-19 15:10:51 -0400

    renamed courses_groups fields

--- web/ptonprowl/courses_groups/models.py
+++ web/ptonprowl/courses_groups/models.py
@@ -3,22 +3,20 @@ from django.db import models
 # course model
 
 class Course(models.Model):
-    number = models.IntegerField(default=0, primary_key=True)
+    id = models.IntegerField(default=0, primary_key=True)
     title = models.CharField(max_length=200, unique=False)
     groups = models.TextField()
 
     def __str__(self):
-        return str(self.number) + ": " + self.title
+        return str(self.id) + ": " + self.title
 
-
-
-class CourseID(models.Model):
-    id = models.CharField(max_length=255, unique=True, primary_key=True)
+class CourseCode(models.Model):
+    code = models.CharField(max_length=255, unique=True, primary_key=True)
     # unique??!
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.id + " - " + self.course.__str__()
+        return self.code + " - " + self.course.__str__()
 
 class Group(models.Model):
     members = models.TextField()
