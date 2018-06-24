7b4a5ae54afcf85edfa26b896dd165bb902a5609
Van Brantley <van.brantley@gmail.com>
2018-06-24 15:55:14 -0400

    Auto stash before merge of "web" and "origin/web"

---
+++ web/ptonprowl/courses_groups/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Course, Code, Group
+
+# Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Code)
+admin.site.register(Group)
--- web/ptonprowl/students/models.py
+++ web/ptonprowl/students/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from core.models import Course, Code, Group
 
 # Create your models here.
 
