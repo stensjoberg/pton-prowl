9823953cb51e9e7a9391b2cbd70748be6cc665aa
Unknown <minaba331@gmail.com>
2018-06-25 11:35:41 -0400

    firebase setup

---
+++ iOS/storyboardproject/Pods/Target Support Files/Pods-storyboardprojectTests/Pods-storyboardprojectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_storyboardprojectTests : NSObject
+@end
+@implementation PodsDummy_Pods_storyboardprojectTests
+@end
---
+++ iOS/storyboardproject/Pods/Target Support Files/Pods-storyboardprojectUITests/Pods-storyboardprojectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_storyboardprojectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_storyboardprojectUITests
+@end
